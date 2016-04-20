5c675f76aa72de5d1ce511dccc89ac63031c2313
Carlin Scuderi <carlinscuderi@gmail.com>
2016-04-19 22:25:16 -0400

    Initial commit

---
+++ diddly-dipsum.rb
@@ -0,0 +1,44 @@
+require 'sinatra'
+require 'JSON'
+
+set :port, 8080
+set :environment, :production
+
+
+# √ 1. Build a 'database' of strings in Flanders' talk
+# √ 2. Prompt user for how many paragraphs of Flanders they want
+#   3. Build paragraphs by pulling strings from the database at random
+#   3.5 Add random dashes between words instead of spaces
+#   4. Cap paragraphs at 40 words, 55 words or 70 words (randomly)
+#   5. Count up until we have the number of paragraphs requested
+#   6. Return the paragraphs
+
+class DiddlyServer
+  doodlies = ['okey', 'dokey', 'dokelees', 'dandy', 'diddly', 'di', 'do', 'iddly', 'ho', 'neighborino', 'ding', 'dong', 'bitty', 'arky', 'twosies', 'purple drapes', 'fella', 'doodly', 'oodly', 'doddily', 'shoodily', 'dahdilly', 'okelly', 'dokely']
+
+  # Verify the input and route the call
+  def initialize
+    # Get the number of requested paragraphs here!
+    return 'Hi neighborino!'
+  end
+
+  # Build the paragraphs of text
+  def getDoodlies(numRequested)
+    counter = 0
+    response = ''
+
+    until counter == numRequested do
+      response += doodlies[rand(doodlies.length)]
+      counter += 1
+    end
+
+    return response
+  end
+end
+
+Diddly = DiddlyServer.new
+
+get '/diddlies' do
+  return 'Heard you'
+  # Diddly.initialize
+end
